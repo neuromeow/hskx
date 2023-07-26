e9ad3ad55fac9a91ec74dd6596a4c89576644e41
neuromeow <92692649+neuromeow@users.noreply.github.com>
2023-07-26 22:05:07 +0500

    Add dependency and simple function to read csv file

--- src/core.rs
+++ src/core.rs
@@ -1,5 +1,19 @@
 use std::error::Error;
 
+use csv;
+
+fn read_from_file(path: &str) -> Result<(), Box<dyn Error>> {
+    let mut reader = csv::Reader::from_path(path)?;
+    let headers = reader.headers()?;
+    println!("{:?}", headers);
+    for record in reader.records() {
+        let unwrapped_record = record?;
+        println!("{:?}", unwrapped_record);
+    }
+    Ok(())
+}
+
 pub fn run() -> Result<(), Box<dyn Error>> {
+    read_from_file("./src/data/wordlist.csv")?;
     Ok(())
 }
