3b549387b80d050fad45ad85520b21c526bf2a24
neuromeow <92692649+neuromeow@users.noreply.github.com>
2023-07-27 17:20:10 +0500

    Add new function with deserialization

--- src/core.rs
+++ src/core.rs
@@ -12,6 +12,15 @@ struct Word {
     hsk_level: u8,
 }
 
+#[derive(Deserialize)]
+struct WordRecord {
+    word_number: u32,
+    chinese: String,
+    pinyin: String,
+    english: String,
+    hsk_level: u8,
+}
+
 #[warn(dead_code)]
 fn read_from_file(path: &str) -> Result<(), Box<dyn Error>> {
     let mut reader = csv::Reader::from_path(path)?;
@@ -36,8 +45,25 @@ fn read_from_file_and_deserialize(path: &str) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+fn read_from_file_deserialize_and_print(path: &str) -> Result<(), Box<dyn Error>> {
+    let mut reader = csv::Reader::from_path(path)?;
+    for record in reader.deserialize() {
+        let word_record: WordRecord = record?;
+        println!(
+            "{} {} {} {} {}",
+            word_record.word_number,
+            word_record.chinese,
+            word_record.pinyin,
+            word_record.english,
+            word_record.hsk_level
+        )
+    }
+    Ok(())
+}
+
 pub fn run() -> Result<(), Box<dyn Error>> {
     // read_from_file("./src/data/wordlist.csv")?;
-    read_from_file_and_deserialize("./src/data/wordlist.csv")?;
+    // read_from_file_and_deserialize("./src/data/wordlist.csv")?;
+    read_from_file_deserialize_and_print("./src/data/wordlist.csv")?;
     Ok(())
 }
