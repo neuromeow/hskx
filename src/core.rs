7cbdff0bdc8d601f7073d283ca75691b2112613d
neuromeow <92692649+neuromeow@users.noreply.github.com>
2023-07-27 18:00:16 +0500

    Refactor existing functions

--- src/core.rs
+++ src/core.rs
@@ -3,7 +3,7 @@ use std::error::Error;
 use csv;
 use serde::Deserialize;
 
-#[derive(Debug, Deserialize)]
+#[derive(Deserialize)]
 struct Word {
     word_number: u32,
     chinese: String,
@@ -12,58 +12,23 @@ struct Word {
     hsk_level: u8,
 }
 
-#[derive(Deserialize)]
-struct WordRecord {
-    word_number: u32,
-    chinese: String,
-    pinyin: String,
-    english: String,
-    hsk_level: u8,
-}
-
-#[warn(dead_code)]
-fn read_from_file(path: &str) -> Result<(), Box<dyn Error>> {
+fn read_from_file_and_deserialize(path: &str) -> Result<Vec<Word>, Box<dyn Error>> {
     let mut reader = csv::Reader::from_path(path)?;
-    let headers = reader.headers()?;
-    println!("{:?}", headers);
-    for record in reader.records() {
-        let unwrapped_record = record?;
-        println!("{:?}", unwrapped_record);
-    }
-    Ok(())
-}
-
-#[warn(dead_code)]
-fn read_from_file_and_deserialize(path: &str) -> Result<(), Box<dyn Error>> {
-    let mut reader = csv::Reader::from_path(path)?;
-    let headers = reader.headers()?;
-    println!("{:?}", headers);
+    let mut result = Vec::new();
     for record in reader.deserialize() {
-        let result: Word = record?;
-        println!("{:?}", result);
+        let word_record: Word = record?;
+        result.push(word_record)
     }
-    Ok(())
-}
-
-fn read_from_file_deserialize_and_print(path: &str) -> Result<(), Box<dyn Error>> {
-    let mut reader = csv::Reader::from_path(path)?;
-    for record in reader.deserialize() {
-        let word_record: WordRecord = record?;
-        println!(
-            "{} {} {} {} {}",
-            word_record.word_number,
-            word_record.chinese,
-            word_record.pinyin,
-            word_record.english,
-            word_record.hsk_level
-        )
-    }
-    Ok(())
+    Ok(result)
 }
 
 pub fn run() -> Result<(), Box<dyn Error>> {
-    // read_from_file("./src/data/wordlist.csv")?;
-    // read_from_file_and_deserialize("./src/data/wordlist.csv")?;
-    read_from_file_deserialize_and_print("./src/data/wordlist.csv")?;
+    let words = read_from_file_and_deserialize("./src/data/wordlist.csv")?;
+    for word in words {
+        println!(
+            "{} {} {} {} {}",
+            word.word_number, word.chinese, word.pinyin, word.english, word.hsk_level
+        )
+    }
     Ok(())
 }
