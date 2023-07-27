2efc08c6c9133926ac893accec4a6444313e1e27
neuromeow <92692649+neuromeow@users.noreply.github.com>
2023-07-27 17:15:29 +0500

    Add a function to read from a file and deserialize

--- src/core.rs
+++ src/core.rs
@@ -1,7 +1,18 @@
 use std::error::Error;
 
 use csv;
+use serde::Deserialize;
 
+#[derive(Debug, Deserialize)]
+struct Word {
+    word_number: u32,
+    chinese: String,
+    pinyin: String,
+    english: String,
+    hsk_level: u8,
+}
+
+#[warn(dead_code)]
 fn read_from_file(path: &str) -> Result<(), Box<dyn Error>> {
     let mut reader = csv::Reader::from_path(path)?;
     let headers = reader.headers()?;
@@ -13,7 +24,20 @@ fn read_from_file(path: &str) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+#[warn(dead_code)]
+fn read_from_file_and_deserialize(path: &str) -> Result<(), Box<dyn Error>> {
+    let mut reader = csv::Reader::from_path(path)?;
+    let headers = reader.headers()?;
+    println!("{:?}", headers);
+    for record in reader.deserialize() {
+        let result: Word = record?;
+        println!("{:?}", result);
+    }
+    Ok(())
+}
+
 pub fn run() -> Result<(), Box<dyn Error>> {
-    read_from_file("./src/data/wordlist.csv")?;
+    // read_from_file("./src/data/wordlist.csv")?;
+    read_from_file_and_deserialize("./src/data/wordlist.csv")?;
     Ok(())
 }
