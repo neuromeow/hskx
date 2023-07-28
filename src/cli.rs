6d24d9385cac2f6a316b6f5da60441ba100de996
neuromeow <92692649+neuromeow@users.noreply.github.com>
2023-07-28 17:00:29 +0500

    Add levels option template

--- src/cli.rs
+++ src/cli.rs
@@ -1,8 +1,11 @@
 use clap::{Parser, Subcommand};
+use std::ops::Range;
 
 #[derive(Parser)]
 #[command(version)]
 pub struct Cli {
+    #[arg(short, long, global = true, value_delimiter = ',', use_value_delimiter = true, value_parser = clap::value_parser!(u8).range(1..7))]
+    pub levels: Option<Vec<u8>>,
     #[command(subcommand)]
     pub command: Commands,
 }
