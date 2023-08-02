9bf67bb642d28bec171c6a4c76a5daad57ef8efe
neuromeow <92692649+neuromeow@users.noreply.github.com>
2023-08-02 19:05:03 +0500

    Implement usage of the answer option

--- src/core.rs
+++ src/core.rs
@@ -63,6 +63,9 @@ pub fn run() -> Result<(), Box<dyn Error>> {
                     }
                     println!("{}\n", printing_string);
                     thread::sleep(delay_duration);
+                    if *answer == true {
+                        println!("{}", word);
+                    }
                 }
             } else {
                 for word in words {
@@ -82,6 +85,9 @@ pub fn run() -> Result<(), Box<dyn Error>> {
                     // As a way to wait for user input
                     let mut buffer = String::new();
                     io::stdin().read_line(&mut buffer)?;
+                    if *answer == true {
+                        println!("{}", word);
+                    }
                 }
             }
         }
