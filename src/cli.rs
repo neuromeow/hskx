ca732d2690a021fb5aab5e89395bf27f6581eb4f
neuromeow <92692649+neuromeow@users.noreply.github.com>
2023-08-08 19:05:33 +0500

    Add a simple unit test to verify command line interface

--- src/cli.rs
+++ src/cli.rs
@@ -32,3 +32,14 @@ pub enum Commands {
         numbers: bool,
     },
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn verify_cli() {
+        use clap::CommandFactory;
+        Cli::command().debug_assert()
+    }
+}
