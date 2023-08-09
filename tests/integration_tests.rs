56ebb0eda65592a65443360b956a3477708ec2ae
neuromeow <92692649+neuromeow@users.noreply.github.com>
2023-08-09 20:00:18 +0500

    Add integration tests for train command

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -58,3 +58,137 @@ fn test_hskx_wordlist_with_levels_for_several_levels_and_numbers_options() {
         .assert()
         .success();
 }
+
+#[test]
+fn test_hskx_train() {
+    create_hskx_train_command().assert().success();
+}
+
+#[test]
+fn test_hskx_train_with_pinyin_option() {
+    create_hskx_train_command()
+        .arg("--pinyin")
+        .assert()
+        .success();
+}
+
+#[test]
+fn test_hskx_train_with_english_option() {
+    create_hskx_train_command()
+        .arg("--english")
+        .assert()
+        .success();
+}
+
+fn test_hskx_train_with_pinyin_and_english_options() {
+    create_hskx_train_command()
+        .arg("--pinyin")
+        .arg("--english")
+        .assert()
+        .success();
+}
+
+#[test]
+fn test_hskx_train_with_no_chinese_option() {
+    create_hskx_train_command()
+        .arg("--no-chinese")
+        .assert()
+        .failure();
+}
+
+#[test]
+fn test_hskx_train_with_no_chinese_and_pinyin_options() {
+    create_hskx_train_command()
+        .arg("--no-chinese")
+        .arg("--pinyin")
+        .assert()
+        .success();
+}
+
+#[test]
+fn test_hskx_train_with_no_chinese_and_english_options() {
+    create_hskx_train_command()
+        .arg("--no-chinese")
+        .arg("--english")
+        .assert()
+        .success();
+}
+
+#[test]
+fn test_hskx_train_with_no_chinese_and_pinyin_and_english_options() {
+    create_hskx_train_command()
+        .arg("--no-chinese")
+        .arg("--pinyin")
+        .arg("--english")
+        .assert()
+        .success();
+}
+
+#[test]
+fn test_hskx_train_with_levels_option_for_one_level() {
+    create_hskx_train_command()
+        .arg("--levels=1")
+        .assert()
+        .success();
+}
+
+#[test]
+fn test_hskx_train_with_levels_option_for_several_levels() {
+    create_hskx_train_command()
+        .arg("--levels=1,2,5")
+        .assert()
+        .success();
+}
+
+#[test]
+fn test_hskx_train_with_answer_option() {
+    create_hskx_train_command()
+        .arg("--answer")
+        .assert()
+        .success();
+}
+
+#[test]
+fn test_hskx_train_with_shuffle_option() {
+    create_hskx_train_command()
+        .arg("--shuffle")
+        .assert()
+        .success();
+}
+
+#[test]
+#[ignore]
+fn test_hskx_train_with_delay_option() {
+    create_hskx_train_command()
+        .arg("--delay=1")
+        .assert()
+        .success();
+}
+
+#[test]
+fn test_hskx_train_with_all_options_except_delay_option() {
+    create_hskx_train_command()
+        .arg("--no-chinese")
+        .arg("--pinyin")
+        .arg("--english")
+        .arg("--levels=3,4,6")
+        .arg("--answer")
+        .arg("--shuffle")
+        .assert()
+        .success();
+}
+
+#[test]
+#[ignore]
+fn test_hskx_train_with_all_options() {
+    create_hskx_train_command()
+        .arg("--no-chinese")
+        .arg("--pinyin")
+        .arg("--english")
+        .arg("--levels=3,4,6")
+        .arg("--answer")
+        .arg("--shuffle")
+        .arg("--delay=1")
+        .assert()
+        .success();
+}
