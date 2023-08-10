18a560dec2168d3d0ccf7d9f4fb94a3af5bc66f6
neuromeow <92692649+neuromeow@users.noreply.github.com>
2023-08-10 21:35:10 +0500

    Update help messages for the command line interface

--- src/cli.rs
+++ src/cli.rs
@@ -1,5 +1,6 @@
 use clap::{Parser, Subcommand};
 
+/// Console application to prepare for the Chinese Proficiency Test, known as The Hanyu Shuiping Kaoshi (HSK)
 #[derive(Parser)]
 #[command(version)]
 pub struct Cli {
@@ -9,25 +10,36 @@ pub struct Cli {
 
 #[derive(Subcommand)]
 pub enum Commands {
+    /// Print words for practice
     Train {
+        /// Exam level
         #[arg(short, long, value_delimiter = ',', use_value_delimiter = true, value_parser = clap::value_parser!(u8).range(1..7))]
         levels: Option<Vec<u8>>,
+        /// Chinese characters are not displayed
         #[arg(short, long)]
         no_chinese: bool,
+        /// Hanyu pinyin are displayed
         #[arg(short, long)]
         pinyin: bool,
+        /// English translations of words are displayed
         #[arg(short, long)]
         english: bool,
+        /// Show the word and it's translation after the question
         #[arg(short, long)]
         answer: bool,
+        /// Words appear out of order
         #[arg(short, long)]
         shuffle: bool,
+        /// Delay between words
         #[arg(short, long, value_parser = clap::value_parser!(u64).range(1..))]
         delay: Option<u64>,
     },
+    /// Print a list of words for exam preparation
     Wordlist {
+        /// Exam level
         #[arg(short, long, value_delimiter = ',', use_value_delimiter = true, value_parser = clap::value_parser!(u8).range(1..7))]
         levels: Option<Vec<u8>>,
+        /// Display the numbers of the words they have in the wordlist
         #[arg(short, long)]
         numbers: bool,
     },
